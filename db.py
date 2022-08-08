98c229af6e707fdf02888aceb68f7b6501762374
Mandalore <serriy_volk@mail.ru>
2022-08-08 22:19:33 +0500

    Add: Сохранение последнего времени запуска и поиск новых отзывов

--- db.py
+++ db.py
@@ -1,9 +1,13 @@
 import peewee
+from datetime import datetime
+
+import pytz
 
 db = peewee.PostgresqlDatabase(database='reviews', user='reviews_user', password='reviews', host='localhost')
 
 
 class Review(peewee.Model):
+    """Модель отзыва"""
     source = peewee.CharField()
     user_name = peewee.CharField()
     date_created = peewee.DateTimeField()
@@ -14,10 +18,18 @@ class Review(peewee.Model):
         database = db
 
 
+class LastRun(peewee.Model):
+    """Последние запуски программы"""
+    datetime = peewee.DateTimeField()
+
+    class Meta:
+        database = db
+
+
 def init_database():
     """Инициализирует базу данных"""
     db.connect()
-    db.create_tables([Review])
+    db.create_tables([LastRun, Review])
 
 
 def close_connection():
@@ -32,3 +44,24 @@ def save_review_to_database(review):
     :param review: Словарь из source, user_name, date_created, rating, text
     """
     Review.create(**review)
+
+
+def get_last_run_datetime():
+    """Возвращает дату и время последнего запуска с часовым поясом +5 Уфа. Если запусков не было, возвращает 2000 год"""
+    try:
+        last_run = LastRun.select().order_by(LastRun.datetime).limit(1).get().datetime
+
+        last_run = last_run.replace(tzinfo=pytz.timezone("Asia/Yekaterinburg"))
+    except peewee.DoesNotExist:
+        last_run = datetime.fromisoformat("2000-01-01T00:00:00+05:00")
+
+    return last_run
+
+
+def save_last_run_datetime(last_run):
+    """
+    Сохраняет время последнего запуска
+
+    :param last_run: Время последнего запуска
+    """
+    LastRun.create(datetime=last_run)
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import requests
 import json
 import db
+from datetime import datetime
 
 
-def handle_reviews_2gis(reviews, firm_id):
+def handle_reviews_2gis(reviews, firm_id, min_date):
     """
     Сохраняет отзывы из полученного списка отзывов. В источник подставляется ссылка на обзоры конкретной фирмы.
 
     :param reviews: Список отзывов
     :param firm_id: Id в 2gis текущей фирмы "Вкусно и точка"
+    :param min_date: Дата, с которой ищутся отзывы
+    :returns: Дошли ли мы до отзыва с меньшей датой, чем min_date
     """
     source = f"https://2gis.ru/ufa/branches/2393074172953395/firm/{firm_id}/tab/reviews"
     for review_data in reviews:
@@ -18,18 +21,24 @@ def handle_reviews_2gis(reviews, firm_id):
         review["date_created"] = review_data["date_created"]
         review["rating"] = review_data["rating"]
         review["text"] = review_data["text"]
+
+        # Удаляем из времени миллисекунды, так как datetime неправильно с ним работает
+        review["date_created"] = review["date_created"][:19] + review["date_created"][-6:]
+        if datetime.fromisoformat(review["date_created"]) < min_date:
+            return False
+
         db.save_review_to_database(review)
 
-        # with open("reviews_2gis.txt", "a", encoding="utf-8") as file:
-        #     file.write(f"{user_name}, {date_created}, {rating}, {text}, {source}\n")
+    return True
 
 
-def handle_firms_2gis(firms, settings):
+def handle_firms_2gis(firms, settings, min_date):
     """
     Сохраняет отзывы для каждой фирмы 'Вкусно и точка'
 
     :param firms: Список фирм
     :param settings: Словарь настроек из settings.json
+    :param min_date: Дата, с которой ищутся отзывы
     """
     for firm in firms:
         # Берем id фирмы и формируем ссылку на данные об отзывах о фирме
@@ -40,7 +49,11 @@ def handle_firms_2gis(firms, settings):
         response = requests.get(link).json()
         while True:
             # Сохраняем информацию
-            handle_reviews_2gis(response["reviews"], firm_id)
+            has_more_reviews = handle_reviews_2gis(response["reviews"], firm_id, min_date)
+
+            # Если сохранили все новые отзывы, выходим
+            if not has_more_reviews:
+                break
 
             # Ссылка на следующую порцию отзывов в атрибуте meta.next_link JSON-файла
             try:
@@ -51,12 +64,14 @@ def handle_firms_2gis(firms, settings):
             response = requests.get(next_link).json()
 
 
-def handle_reviews_flamp(reviews, firm_id):
+def handle_reviews_flamp(reviews, firm_id, min_date):
     """
     Сохраняет отзывы из полученного списка отзывов. В источник подставляется ссылка на обзоры конкретной фирмы.
 
     :param reviews: Список отзывов
     :param firm_id: Id в flamp текущей фирмы "Вкусно и точка"
+    :param min_date: Дата, с которой ищутся отзывы
+    :returns: Дошли ли мы до отзыва с меньшей датой, чем min_date
     """
     source = f"https://ufa.flamp.ru/firm/vkusno_i_tochka-{firm_id}"
     for review_data in reviews:
@@ -66,18 +81,26 @@ def handle_reviews_flamp(reviews, firm_id):
         review["date_created"] = review_data["date_created"]
         review["rating"] = review_data["rating"]
         review["text"] = review_data["text"]
+
+        if datetime.fromisoformat(review["date_created"]) < min_date:
+            return False
+
         db.save_review_to_database(review)
 
+    return True
 
-def handle_firms_flamp(firms, settings):
+
+def handle_firms_flamp(firms, settings, min_date):
     """
     Сохраняет отзывы для каждой фирмы 'Вкусно и точка'
 
     :param firms: Список фирм
     :param settings: Словарь настроек из settings.json
+    :param min_date: Дата, с которой ищутся отзывы
     """
     authorization = settings["flamp_authorization"]
     accept = ';q=1;depth=1;scopes={"user":{"fields":"id,name,url,image,reviews_count,sex"},"official_answer":{},"photos":{}};application/json'
+
     for firm in firms:
         # Берем id фирмы и формируем ссылку на данные об отзывах о фирме
         firm_id = firm["id"]
@@ -86,8 +109,13 @@ def handle_firms_flamp(firms, settings):
         # В цикле последовательно получаем информацию об отзывах
         while True:
             response = requests.get(link, headers={"authorization": authorization, "Accept": accept}).json()
+
             # Сохраняем информацию
-            handle_reviews_flamp(response["reviews"], firm_id)
+            has_more_reviews = handle_reviews_flamp(response["reviews"], firm_id, min_date)
+
+            # Если сохранили все новые отзывы, выходим
+            if not has_more_reviews:
+                break
 
             # Ссылка на следующую порцию фирм в атрибуте next_link JSON-файла
             try:
@@ -97,20 +125,29 @@ def handle_firms_flamp(firms, settings):
 
 
 def main():
-    with open("settings.json", "r") as settings_file:
-        settings = json.load(settings_file)
-
     # Соединяемся с базой данных
     db.init_database()
 
-    # # Забираем информацию о фирмах 2gis
-    # # С User-Agent Mozilla Firefox работает, с Google Chrome почему-то не работает
-    # user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0"
-    # response = requests.get(settings["2gis_firms_link"], headers={"User-Agent": user_agent}).json()
-    #
-    # # Информация о фирмах в result.items JSON-файла
-    # firms = response["result"]["items"]
-    # handle_firms_2gis(firms, settings)
+    # min_date = datetime.fromisoformat("2022-08-04 11:25:08+03:00")
+    min_date = db.get_last_run_datetime()
+
+    with open("settings.json", "r") as settings_file:
+        settings = json.load(settings_file)
+
+    # Забираем информацию о фирмах 2gis
+    # С User-Agent Mozilla Firefox работает, с Google Chrome почему-то не работает
+    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0"
+    referer = "https://2gis.ru/"
+    response = requests.get(settings["2gis_firms_link"], headers={"User-Agent": user_agent, "Referer": referer}).json()
+
+    # Если шлет 403 ошибку, забираем данные из существующего файла
+    if response["meta"]["code"] != 200:
+        with open("2gis_firms.json", "r") as firms_file:
+            response = json.load(firms_file)
+
+    # Информация о фирмах в result.items JSON-файла
+    firms = response["result"]["items"]
+    handle_firms_2gis(firms, settings, min_date)
 
     # Забираем информацию о фирмах flamp
     # Authorization должен быть в заголовках, чтобы не кидало 403 ошибку
@@ -126,7 +163,10 @@ def main():
         except KeyError:
             break
 
-    handle_firms_flamp(firms, settings)
+    handle_firms_flamp(firms, settings, min_date)
+
+    # Сохраняем последнее время запуска
+    db.save_last_run_datetime(datetime.now())
 
     # Закрываем базу данных
     db.close_connection()
